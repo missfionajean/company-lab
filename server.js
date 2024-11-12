2f39bebd0ab4a676aa8ad0691026dce59e3a4800
C0R4L0V3 <coraline.e.love@gmail.com>
2024-11-12 15:04:18 -0600

    menu object

--- server.js
+++ server.js
@@ -11,6 +11,51 @@ app.use(express.static('public'));
 
 const PORT = 3000;
 
+
+// menu object array
+
+const menu = {
+    
+    burgerOTDAY: [
+        {day: "Sunday", burger: "placeholder1", desc: "placeholder1", img: url(), price: 15.00 },
+        {day: "Monday", burger: "placeholder2", desc: "placeholder2", price: 15.00 },
+        {day: "Tuesday", burger: "placeholder3", desc: "placeholder3", price: 15.00 },
+        {day: "Wednesday", burger: "placeholder4", desc: "placeholder4", price: 15.00 },
+        {day: "Thursday", burger: "placeholder5", desc: "placeholder5", price: 15.00 },
+        {day: "Friday", burger: "placeholder6", desc: "placeholder6", price: 15.00 },
+        {day: "Saturday", burger: "placeholder7", desc: "placeholder7", price: 15.00 },
+    ],
+
+    mains: [
+        {item: 1, entree: "placeholde1", desc: "placeholder1", price: 99.99},
+        {item: 2, entree: "placeholde2", desc: "placeholder2", price: 99.99},
+        {item: 3, entree: "placeholde3", desc: "placeholder3", price: 99.99},
+        {item: 4, entree: "placeholde4", desc: "placeholder4", price: 99.99},
+    ],
+
+    sides: [
+        {item: 1, side: "placeholde1", desc: "placeholder1", price: 99.99},
+        {item: 2, side: "placeholde2", desc: "placeholder2", price: 99.99},
+        {item: 3, side: "placeholde3", desc: "placeholder3", price: 99.99},
+        {item: 4, side: "placeholde4", desc: "placeholder4", price: 99.99},
+    ],
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
     console.log('Test');
     res.render('home.ejs')   
