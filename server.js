0ee5d8a641a6b0db9555963378827856b8fa0d24
C0R4L0V3 <coraline.e.love@gmail.com>
2024-11-12 12:21:29 -0600

    updated framework for project

--- server.js
+++ server.js
@@ -1,2 +1,41 @@
 const express = require('express')
 const app = express()
+
+// setting up morgan for better HTTP logging
+/* remember this is middleware that must be installed */
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+//tells express to try to match requests with files in the directory called 'public'
+app.use(express.static('public')); 
+
+const PORT = 3000;
+
+app.get('./home', (req, res) => {
+    console.log('Test');
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT);
