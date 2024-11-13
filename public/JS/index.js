d318e7c2f027422fa85016f51e3bc8f1d529e3ce
C0R4L0V3 <coraline.e.love@gmail.com>
2024-11-12 18:46:01 -0600

    BOTD daily cylce working

---
+++ public/JS/index.js
@@ -0,0 +1,12 @@
+// const botd = document.getElementById('botdImg')
+
+const date = new Date()
+const dayOfWeek = date.getDay()
+
+
+// console.log(dayOfWeek)
+
+// updateImage = () => {
+//     const today = new Date();
+//     const imageName=   ``
+// }
